f5610e92a2da325744d0f0c2433de345e9b814e8
SanJin <sanjinhub@gmail.com>
2026-07-03 23:57:54 +0800

    update to v1.0.0

--- Formula/coot.rb
+++ Formula/coot.rb
@@ -5,10 +5,10 @@ class Coot < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/coot-ai/coot-cli/releases/download/v1.0.0/coot_v1.0.0_darwin_arm64.tar.gz"
-    sha256 "c82f6b28589a14e86a557865f64a1a01af68b13578de66577f30a6398134d55b"
+    sha256 "3ce05415b478a2d3bfaf9ccf333e61e35df2330d509ef40d8a6077ee438eedce"
   else
     url "https://github.com/coot-ai/coot-cli/releases/download/v1.0.0/coot_v1.0.0_darwin_amd64.tar.gz"
-    sha256 "b8b2fb1cdfa2f302b47f576a34a78bd542e9355ef853241507ebc6119d8d0149"
+    sha256 "feff2f9fbc40c616b66a2b8aeb9e65dbc64fba22a571119b7293edd9272d0af2"
   end
 
   def install
