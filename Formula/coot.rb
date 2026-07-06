e79266790de116a127cdf49ffebe0497d3ed23ae
SanJin <sanjinhub@gmail.com>
2026-07-06 15:30:35 +0800

    update to v0.2.0

--- Formula/coot.rb
+++ Formula/coot.rb
@@ -1,14 +1,14 @@
 class Coot < Formula
   desc "Package Codex/Claude-built iOS apps for device testing, anywhere."
   homepage "https://coot.dev"
-  version "0.1.0"
+  version "0.2.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/coot-ai/coot-cli/releases/download/v0.1.0/coot_v0.1.0_darwin_arm64.tar.gz"
-    sha256 "67284cf177e102ffd1a3b363a4a1c08dfd5cb944e80eede0588d7010c8fb479b"
+    url "https://github.com/coot-ai/coot-cli/releases/download/v0.2.0/coot_v0.2.0_darwin_arm64.tar.gz"
+    sha256 "b1ac591020d10dcb2dd6cc36ee35a4fc6e4afae54fe9a174a5f1eeb2ea111d00"
   else
-    url "https://github.com/coot-ai/coot-cli/releases/download/v0.1.0/coot_v0.1.0_darwin_amd64.tar.gz"
-    sha256 "e0e31eaffb5be7a55ff39255fc13281ec49b72ba8702c3fe25c0aee9dec643e3"
+    url "https://github.com/coot-ai/coot-cli/releases/download/v0.2.0/coot_v0.2.0_darwin_amd64.tar.gz"
+    sha256 "81e503cb2ce980b8442aa665053587dd4066072f6d46c95fc03438397452b3ae"
   end
 
   def install
